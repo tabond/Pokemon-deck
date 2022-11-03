91e34bafd7d473be9ef5ff1d0948b858fb63665a
tabond <30688315+tabond@users.noreply.github.com>
2022-11-02 20:05:14 -0700

    loop on names and height added

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ let pokemonList= [
     { name: 'Pidgey', height: 3, types: ['Flying', 'Normal']},
     { name: 'Pikachu', height: 4, types: ['Electric', 'Poison']},
     { name: 'Charizard', height: 17, types: ['Fire', 'Flying']}
-  ];
+  ]
+  //returns list and heigh of pokemon
+  let sizequote = ''
+  for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 6) {
+        sizequote = " - phwaaa thats a big one!";}
+        else {
+            sizequote = " - ahhh cute! :)";
+        }
+    document.write([pokemonList[i].name +', '+ 'Height =' + pokemonList[i].height + sizequote ]);
+    document.write("<br><br>")
+    }
