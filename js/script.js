c5fb5fe622ff3a0f84a5bada0d3925f954971e37
tabond <30688315+tabond@users.noreply.github.com>
2022-11-04 18:41:27 -0700

    changed for to for each

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 
 let pokemonList= [
     { name: 'Bulbasaur', height: 7, types:['Grass', 'Poison']},
-    { name: 'Charmander', height: 6, types: ['Fire']},
-    { name: 'Squirtle', height: 5, types: ['Water']},
-    { name: 'Pidgey', height: 3, types: ['Flying', 'Normal']},
     { name: 'Pikachu', height: 4, types: ['Electric', 'Poison']},
     { name: 'Charizard', height: 17, types: ['Fire', 'Flying']}
   ]
+  let pokemonList2= [
+    { name: 'Charmander', height: 6, types: ['Fire']},
+    { name: 'Squirtle', height: 5, types: ['Water']},
+    { name: 'Pidgey', height: 3, types: ['Flying', 'Normal']},
+  ]
   //returns list and heigh of pokemon
   let sizequote = ''
-  for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 6) {
-        sizequote = " - phwaaa thats a big one!";}
-        else {
-            sizequote = " - ahhh cute! :)";
-        }
-    document.write([pokemonList[i].name +', '+ 'Height =' + pokemonList[i].height + sizequote ]);
-    document.write("<br><br>")
+
+function pokemon_detail(pokemon) {
+        if (pokemon.height > 6) {
+            sizequote = " - phwaaa thats a big one!";
+        } else {
+                sizequote = " - ahhh cute! :)";
+            }
+    
+        document.write([
+            pokemon.name +', '+ 'Height =' + pokemon.height + sizequote 
+        ]);
+    
+        document.write("<br><br>");
     }
+        pokemonList.forEach(pokemon_detail);
+        pokemonList2.forEach(pokemon_detail);
+
+
+
+
