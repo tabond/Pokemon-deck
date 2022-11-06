d83752515cc0337b219898ad2e62d16873ac1a0a
tabond <30688315+tabond@users.noreply.github.com>
2022-11-05 21:03:53 -0700

    changed for an IIFE function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,46 @@
 
-let pokemonList= [
-    { name: 'Bulbasaur', height: 7, types:['Grass', 'Poison']},
-    { name: 'Pikachu', height: 4, types: ['Electric', 'Poison']},
-    { name: 'Charizard', height: 17, types: ['Fire', 'Flying']}
-  ]
-  let pokemonList2= [
-    { name: 'Charmander', height: 6, types: ['Fire']},
-    { name: 'Squirtle', height: 5, types: ['Water']},
-    { name: 'Pidgey', height: 3, types: ['Flying', 'Normal']},
-  ]
-  //returns list and heigh of pokemon
-  let sizequote = ''
+  function pokemon_detail(pokemon) {
+    if (pokemon.height > 6) {
+        sizequote = " - phwaaa thats a big one!";
+    } else {
+            sizequote = " - ahhh cute! :)";
+        }
 
-function pokemon_detail(pokemon) {
-        if (pokemon.height > 6) {
-            sizequote = " - phwaaa thats a big one!";
-        } else {
-                sizequote = " - ahhh cute! :)";
-            }
-    
-        document.write([
-            pokemon.name +', '+ 'Height =' + pokemon.height + sizequote 
-        ]);
-    
-        document.write("<br><br>");
-    }
-        pokemonList.forEach(pokemon_detail);
-        pokemonList2.forEach(pokemon_detail);
+    document.write([
+        pokemon.name +', '+ 'Height =' + pokemon.height + sizequote 
+    ]);
 
+    document.write("<br><br>");
+}
 
+      let pokemonRepository = (function () {
+        let pokemonList = [
+            { name: 'Bulbasaur', height: 7, types:['Grass', 'Poison']},
+            { name: 'Pikachu', height: 4, types: ['Electric', 'Poison']},
+            { name: 'Charizard', height: 17, types: ['Fire', 'Flying']},
+            { name: 'Charmander', height: 6, types: ['Fire']},
+            { name: 'Squirtle', height: 5, types: ['Water']},
+            { name: 'Pidgey', height: 3, types: ['Flying', 'Normal']},
+        ];
+        sizequote =''
+      
+        function add(pokemon) {
+          pokemonList.push(pokemon);
+        }
+        function getAll() {
+          return pokemonList;
+        }
+      
+        return {
+          add: add,
+          getAll: getAll,
+        };
+      })()
+
+//how come you can log the array to the consol but not document write it
+console.log(pokemonRepository.getAll()); 
+pokemonRepository.add({ name: 'Pikachu', height: 100, type: 'Electric' });
+console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(pokemon_detail);
 
 
